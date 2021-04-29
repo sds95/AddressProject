e9cd6b451bede66036eafb7fe0cb1b39bee45429
SDS <SDS@SDS>
2021-04-29 21:54:38 +0300

    add project

---
+++ Address.cs
@@ -0,0 +1,79 @@
+﻿namespace AddressProject
+{
+    class Address
+    {
+        private int index;
+        private string country;
+        private string city;
+        private string street;
+        private int house;
+        private int apartment;
+
+        public int Index 
+        {
+            get
+            {
+                return index;
+            }
+            set
+            {
+                index = value;
+            }
+        }
+        public string Country
+        {
+            get
+            {
+                return country;
+            }
+            set
+            {
+                country = value;
+            }
+        }
+        public string Street
+        {
+            get
+            {
+                return street;
+            }
+            set
+            {
+                street = value;
+            }
+        }
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+            }
+        }
+        public int House
+        {
+            get
+            {
+                return house;
+            }
+            set
+            {
+                house = value;
+            }
+        }
+        public int Apartment
+        {
+            get
+            {
+                return apartment;
+            }
+            set
+            {
+                apartment = value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AddressProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите индекс: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите страну: ");
+            string country = Console.ReadLine();
+            Console.WriteLine("Введите город: ");
+            string city = Console.ReadLine();
+            Console.WriteLine("Введите улицу: ");
+            string street = Console.ReadLine();
+            Console.WriteLine("Введите дом: ");
+            int house = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите квартиру: ");
+            int apartment = Convert.ToInt32(Console.ReadLine());
+
+            Address address = new Address();
+            address.Index = index;
+            address.Country = country;
+            address.City = city;
+            address.Street = street;
+            address.House = house;
+            address.Apartment = apartment;
+
+            Console.WriteLine("Посылка отправлена по адресу: " 
+                + address.Index + " "
+                + address.Country + " "
+                + address.City + " "
+                + address.Street + " "
+                + address.House + " "
+                + address.Apartment);
+
+            Console.ReadKey();
+        }
+    }
+}
